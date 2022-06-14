3aba760f91faf1dd4a3f6f210df76275925f87fd
ivagavran <ivagavran14@gmail.com>
2022-06-14 17:55:40 +0200

    add project

---
+++ Iva_Gavran_MTTPP/Iva_Gavran_MTTPP/UnitTest1.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class IvaGavranTestCase
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheIvaGavranTestCaseTest()
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.FindElement(By.Id("user-name")).Click();
+            driver.FindElement(By.Id("user-name")).Clear();
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).Click();
+            driver.FindElement(By.Id("password")).Clear();
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("password")).SendKeys(Keys.Enter);
+            driver.FindElement(By.Id("add-to-cart-test.allthethings()-t-shirt-(red)")).Click();
+            driver.FindElement(By.LinkText("1")).Click();
+            driver.FindElement(By.Id("remove-test.allthethings()-t-shirt-(red)")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
